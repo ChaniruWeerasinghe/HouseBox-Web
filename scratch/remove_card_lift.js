a20e8719e5b21b85ba1726dafbf83b2436deba90
Chaniru Weerasinghe <chanirub2003work@gmail.com>
2026-07-15 19:20:23 +0530

    Cleaned up the project by removing the scratch folder containing all the old generator scripts

--- scratch/remove_card_lift.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('index.html', 'utf-8');
-
-const before = (c.match(/hover:-translate-y-1/g) || []).length;
-c = c.split(' hover:-translate-y-1').join('');
-const after = (c.match(/hover:-translate-y-1/g) || []).length;
-
-fs.writeFileSync('index.html', c);
-console.log('Removed hover:-translate-y-1 instances:', before - after);
