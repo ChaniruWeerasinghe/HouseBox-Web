1b085ec0d2dc377e120e3c97e3ac995c5b286ba8
Chaniru Weerasinghe <chanirub2003work@gmail.com>
2026-07-15 17:51:38 +0530

    Removed the old hardcoded decorative arrow from the Hero section layout.

--- scratch/clean.js
+++ scratch/clean.js
@@ -11,13 +11,7 @@ let end = c.indexOf(marker2);
 
 if (start !== -1 && end !== -1) {
     // Keep everything up to marker1 + the rest of that section
-    const keepStart = c.substring(0, start) + `        <!-- Decorative Scroll/Arrow Element on Bottom Right -->
-        <div class="absolute bottom-10 right-12 opacity-80 pointer-events-none">
-            <svg width="40" height="40" viewBox="0 0 100 100" fill="none" stroke="black" stroke-width="3">
-                <path d="M50 70 V30 M35 45 L50 30 L65 45" stroke-linecap="round" stroke-linejoin="round"/>
-                <path d="M70 20 Q 90 20, 90 40" stroke-linecap="round"/>
-            </svg>
-        </div>
+    const keepStart = c.substring(0, start) + `
     </section>
 
 `;
